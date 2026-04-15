1cddc8a4c0a48576d1a92028930bcd7c1b0e175b
Guilherme <guilhermesilvalealll@gmail.com>
2026-04-15 17:29:34 -0300

    chore: adiciona estrutura inicial do modulo-a

---
+++ modulo-a/src/main/java/com/anbima/modulo_a/ModuloAApplication.java
@@ -0,0 +1,13 @@
+package com.anbima.modulo_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAApplication.class, args);
+	}
+
+}
---
+++ modulo-a/src/test/java/com/anbima/modulo_a/ModuloAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anbima.modulo_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

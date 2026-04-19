50f8a483aeb46ed06561384eba5eec8d8df13635
Guilherme <guilhermesilvalealll@gmail.com>
2026-04-19 03:02:14 -0300

    chore(modulo-b): adiciona estrutura inicial do modulo B

---
+++ modulo-b/src/main/java/com/anbima/modulo_b/ModuloBApplication.java
@@ -0,0 +1,13 @@
+package com.anbima.modulo_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloBApplication.class, args);
+	}
+
+}
